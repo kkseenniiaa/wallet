9f93e81b93c2d7bb30394bab0678f8b7220a8a62
kkseenniiaa <98638900+kkseenniiaa@users.noreply.github.com>
2023-05-20 00:06:46 +0500

    Add files via upload

--- java.js
+++ java.js
@@ -1,7 +1,25 @@
 let btn = document.querySelector('.btn');
-let input = document.querySelector('.block3__in');
-let price = document.querySelector('.block3__in1');
+let input = document.querySelector('.name1');
+let name = document.querySelector('.name');
+let price = document.querySelector('.price');
+let category = document.querySelector('.category');
+let input1 = document.querySelector('.name2');
+let select = document.querySelector('.select');
 
 btn.addEventListener('click', () =>{
-    
+    let element = document.createElement('li');
+    element.textContent = input.value;
+    name.append(element);
+});
+
+btn.addEventListener('click', ()=>{
+    let element = document.createElement('li');
+    element.textContent = input1.value;
+    price.append(element);
+});
+
+btn.addEventListener('click', ()=>{
+    let element = document.createElement('li');
+    element.textContent = select.value;
+    category.append(element);
 })
